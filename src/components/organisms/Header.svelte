6e0a38e0f41164aed617f50292a47db1ea5197e7
aliyome <aliyome@gmail.com>
2021-05-03 23:30:25 +0900

    feat: add Header

---
+++ src/components/organisms/Header.svelte
@@ -0,0 +1,5 @@
+<header class="top-0 lef-0 w-full z-40 bg-gray-900 shadow fixed border-b border-gray-200">
+	<div class="container mx-auto px-6 h-16 flex justify-between items-center">
+		<span class="font-semibold text-xl text-white tracking-tight"> Picron </span>
+	</div>
+</header>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,5 @@
-<h1>Picron</h1>
-<section>
-	<p class="text-blue-600 underline">Picron is a multiplayer Nonogram.</p>
-</section>
-<button type="button" class="rounded border bg-gray-200 hover:bg-gray-600">Play</button>
+<script>
+	import Header from '../components/organisms/Header.svelte';
+</script>
 
-<div>game window here</div>
+<Header />

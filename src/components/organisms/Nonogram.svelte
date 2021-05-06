0066ed291e9f74075f522bedf0ec0edda79173bd
aliyome <aliyome@gmail.com>
2021-05-06 21:12:49 +0900

    feat: display hints

--- src/components/organisms/Nonogram.svelte
+++ src/components/organisms/Nonogram.svelte
@@ -26,15 +26,80 @@
 	};
 </script>
 
-<div class="grid" style="--width:{puzzle.width}; --height:{puzzle.height};">
-	{#each puzzle.state as type, i}
-		<div class="cell {getBorderClass(i)}">
-			<Cell {type} />
+<div class="layout">
+	<!-- topleft space -->
+	<div />
+
+	<!-- top hint row -->
+	<div class="hint-row" style="--width:{puzzle.width}; --height:1;">
+		{#each puzzle.columnHints as hints, x}
+			<div
+				class="hint-cell flex flex-col items-center justify-end leading-none {getBorderClass(x)}"
+			>
+				{#each hints as hint, y}
+					<div>{hint}</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
+
+	<!-- left hint column -->
+	<div>
+		<div class="hint-col" style="--width:{1}; --height:{puzzle.height};">
+			{#each puzzle.rowHints as hints, y}
+				<div class="hint-cell flex items-center justify-end leading-none border-left border-top">
+					{#each hints as hint, x}
+						<div>{hint}</div>
+					{/each}
+				</div>
+			{/each}
 		</div>
-	{/each}
+	</div>
+
+	<!-- down right nonogram grid -->
+	<div class="grid" style="--width:{puzzle.width}; --height:{puzzle.height};">
+		{#each puzzle.state as type, i}
+			<div class="cell {getBorderClass(i)}">
+				<Cell {type} />
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
+	.layout {
+		display: grid;
+		grid-template-columns: auto 1fr;
+		grid-template-rows: auto 1fr;
+	}
+
+	.hint-row {
+		--border: 1px solid black;
+		--border-thick: 2px solid black;
+		--size: 20px;
+
+		display: grid;
+		grid-template-columns: repeat(var(--width), var(--size));
+		grid-template-rows: auto;
+	}
+
+	.hint-col {
+		--border: 1px solid black;
+		--border-thick: 2px solid black;
+		--size: 20px;
+
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: repeat(var(--height), var(--size));
+	}
+	.hint-cell {
+		border-right: var(--border);
+		border-bottom: var(--border);
+		width: 100%;
+		height: 100%;
+		line-height: 0.9;
+	}
+
 	.grid {
 		--border: 1px solid black;
 		--border-thick: 2px solid black;
